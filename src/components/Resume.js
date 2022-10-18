3f1f85c349a77d585ac5c6f549304459b493907b
Kainat Farooq <kainatfarooqmunara@gmail.com>
2022-10-18 18:46:04 +0500

    updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Resume(){
   return(
     <section className="Resume-box" id="resume">
-      <h1 className="resume">RESUME</h1>
+      <a href="C:/Users/THE LAPTOP STORE/OneDrive/Documents/My Resume .pdf"></a><h1 className="resume">RESUME</h1>
         </section>
   )
 }
