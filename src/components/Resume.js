409b2d7c5285056ce3a75822df1421164b0447cf
Kainat Farooq <kainatfarooqmunara@gmail.com>
2022-10-21 19:13:37 +0500

    updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Resume(){
   return(
     <section className="Resume-box" id="resume">
-      <a href="https://drive.google.com/file/d/1mPGMSNLoEkM0LeTp8vVBeInewZliwrVS/view?usp=sharing" className="resume-link">
+      <a href="https://drive.google.com/file/d/1mPGMSNLoEkM0LeTp8vVBeInewZliwrVS/view?usp=sharing" className="resume-link" target="_blank">
         <h1 className="resume">RESUME</h1></a>
         </section>
   )
