1ec9d5b7f2a4dd2300be5fc80cb68be02dcee715
Kainat Farooq <kainatfarooqmunara@gmail.com>
2023-03-11 18:26:02 +0500

    commit .

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -12,13 +12,5 @@ export default function Main(){
     </section>
   )
 }
-/*<section className="main-section">
-      <div className="main-container">
-      <span className="front-head">Port</span>
-      <span className="front-h2">Folio.</span>*
-      <span className="owner">By Kainat Farooq</span>
-        <div className="btn-div">
-        <a href="#about"><button className="main-btn">click me</button></a>
-        </div>
-        </div>
-    </section>*/
+
+      
